26c40046ce159fe6e950529215a8fc85cb11aa21
Ilya Danilkin <nezorflame@gmail.com>
2026-05-04 15:52:50 +0400

    Add kdeconnect cask

---
+++ Casks/kdeconnect.rb
@@ -0,0 +1,28 @@
+cask "kdeconnect" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "6171"
+  sha256 arm:   "06af6edbf7107fb9beeae6a2e88e9118ef05a18c43af854ce1bd35b1597f2d37",
+         intel: "d86e3fadca6331660abdbf81ea5d5b7e186ea057ea76a090871a4329e1b4631b"
+
+  url "https://origin.cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/kdeconnect-kde-master-#{version}-macos-clang-#{arch}.dmg"
+  name "KDE Connect"
+  desc "Multi-platform device integration: file sharing, notifications, remote input"
+  homepage "https://kdeconnect.kde.org/"
+
+  livecheck do
+    url "https://origin.cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-#{arch}/"
+    regex(/kdeconnect-kde-master[._-](\d+)-macos-clang-#{arch}\.dmg/i)
+  end
+
+  app "KDE Connect.app"
+
+  zap trash: [
+    "~/Library/Application Support/kdeconnect",
+    "~/Library/Caches/KDE/kdeconnect",
+    "~/Library/Logs/KDE/kdeconnect",
+    "~/Library/Preferences/kdeconnectrc",
+    "~/Library/Preferences/org.kde.kdeconnect.plist",
+    "~/Library/Saved Application State/org.kde.kdeconnect.savedState",
+  ]
+end
